85b31be151806d06ea0c3dd6a38b4eb7549b4e70
trabnett <tim@singlekey.com>
2026-04-03 21:27:09 -0400

    update config

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,2 +1,11 @@
-const String apiBaseUrl = 'http://localhost:8181/api';
-const String wsBaseUrl = 'ws://localhost:8181/ws';
+import 'dart:html' as html;
+
+String get _host {
+  final hostname = html.window.location.hostname ?? 'localhost';
+  return hostname == 'localhost' ? 'localhost:8181' : html.window.location.host;
+}
+
+String get _scheme => html.window.location.protocol.replaceAll(':', '');
+
+String get apiBaseUrl => '$_scheme://$_host/api';
+String get wsBaseUrl => '${_scheme == 'https' ? 'wss' : 'ws'}://$_host/ws';
